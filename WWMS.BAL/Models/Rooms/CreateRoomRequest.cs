708b7f9a53cb2ac60bf8092bdbdf0ca38491d496
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-10-18 15:50:45 +0700

    Feature/update-create-room

--- WWMS.BAL/Models/Rooms/CreateRoomRequest.cs
+++ WWMS.BAL/Models/Rooms/CreateRoomRequest.cs
@@ -5,6 +5,5 @@
         public string RoomName { get; set; } = null!;
         public string? LocationAddress { get; set; }
         public int? Capacity { get; set; }
-        public string ManagerName { get; set; } = string.Empty;
     }
 }
